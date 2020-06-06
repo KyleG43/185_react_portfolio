8d03b0f0d4258d4bddf3f565fbd1d24f7fba5976
Kyle Giannini <kylgia@gmail.com>
2020-06-05 23:40:13 -0700

    Got nodes to appear with links

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -13,27 +13,41 @@ firebase.database().ref('movies').child('GraphViz').on('value', snapshot => {
     const state = snapshot.val();
     for (var key in state){
         const movie = state[key];
-        nodes.push({data: movie.Poster, radius: 100});
+        const movieObject = {data: movie.Poster, radius: 100};
+        nodes.push(movieObject);
         const actors = movie.Actors.split(', ');
         for (var i=0; i<actors.length; i++){
+            const actorObject = {data: actors[i], radius: 50};
             if (!nodes.includes(actors[i])){
-                nodes.push({data: actors[i], radius: 50});
+                nodes.push(actorObject);
             }
             //add link between movie and actor
-            links.push({source: nodes.indexOf(actors[i]), target: nodes.indexOf(movie.Poster)});
+            links.push({source: nodes.indexOf(actorObject), target: nodes.indexOf(movieObject)});
         }
     }
 })
 
-function Graph(){
 
+function Graph(){
+    console.log(links)
     function chart(){
         //svg stuff
+        const width = 1920;
+        const height = 1080;
+
         const obj_nodes = nodes.map(d => Object.create(d));
         const obj_links = links.map(d => Object.create(d));
 
         const svg = d3.create("svg")
-            .attr("viewBox", [0, 0, 1920, 1080]);
+            .attr("viewBox", [0, 0, width, height]);
+
+        const link = svg.append("g")
+            .attr("stroke", "#999")
+            .attr("stroke-opacity", 0.6)
+            .selectAll("line")
+            .data(obj_links)
+            .join("line")
+            .attr("stroke-width", 1);
 
         const node = svg.append("g")
             .attr("stroke", "#fff")
@@ -44,13 +58,24 @@ function Graph(){
             .attr("r", d => d.radius)
             .attr("fill", d3.color("steelblue"));
 
-        const link = svg.append("g")
-            .attr("stroke", "999")
-            .attr("stroke-opacity", 0.6)
-            .selectAll("line")
-            .data(obj_links)
-            .join("line")
-            .attr("stroke-width", 1)
+        console.log(nodes)
+        console.log(links)
+        const simulation = d3.forceSimulation(obj_nodes)
+            .force("link", d3.forceLink().links(links).id(d => { return d.index; }).distance(200))
+            .force("charge", d3.forceManyBody())
+            .force("center", d3.forceCenter(width / 2, height / 2));
+        
+        simulation.on("tick", () => {
+            link
+                .attr("x1", d => d.source.x)
+                .attr("y1", d => d.source.y)
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y);
+
+            node
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y);
+        });
 
         return svg.node()
     }
@@ -58,7 +83,6 @@ function Graph(){
     useEffect(() => {
         const elem = document.getElementById("graph");
         elem.appendChild(chart());
-
     })
     
     return <div id="graph"/>
