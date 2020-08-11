d13c7ee20d48e3b5580e604fb67ac95cac82c353
Kyle Giannini <kylgia@gmail.com>
2020-08-10 18:51:28 -0700

    Updated projects list

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
+import Plane from '../images/plane.ico';
+import Spartify from '../images/spartify_logo.png';
 import Spring from '../images/springboot.png';
 
 export class Projects extends Component {
   render() {
     return (
       <div className="projects">
-        <div className="where">
+        <div className="project">
+        <img src={Plane}/>
+          <p>
+            Trip Planner: A Ruby on Rails web app with a React front end that my girlfriend and I are
+            making that helps users plan vacations and other trips.
+          </p>
+        </div>
+        <div className="project">
+        <img src={Spartify}/>
+          <p>
+            Spartify: An Android app that I made in college that utilizes the Spotify API to let users host
+            party queues that guests can join and add songs to from their devices.
+          </p>
+        </div>
+        <div className="project">
           <img src={Spring}/>
           <p>
             <a href="https://ucsb-cs56-where-is-every1.herokuapp.com/" target="_blank">Where Is Every1</a>:
-            A web app my friends and I made to see when UCSB classrooms aren't being used</p>
-          </div>
+            A Spring app my friends and I made to see when UCSB classrooms are empty so we could use the space to study.
+          </p>
+        </div>
       </div>
     );
   }
--- src/components/Tab_List.js
+++ src/components/Tab_List.js
@@ -3,8 +3,8 @@ import Tab from './Tab'
 
 export class Tab_List extends Component {
   render() {
-    return this.props.tabs.map((tab) => (
-      <Tab tab={tab} activeTab = {this.props.activeTab} changeTab={this.props.changeTab}/>
+    return this.props.tabs.map((tab, index) => (
+      <Tab key={index} tab={tab} activeTab = {this.props.activeTab} changeTab={this.props.changeTab}/>
     ));
   }
 }
